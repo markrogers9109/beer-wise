1a5a94a612fb83e7e22d0f6e9e92363ab1cb5380
Mark <mark@automaticclients.com>
2016-09-14 16:44:36 -0700

    three beers one cup

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
-window.onload = function () {
-    var $ = jQuery,
-        wheelPosition = 0;
+var $ = jQuery,
+    wheelPosition = 0;
+
+$(document).ready(function() {
 
     $('#Top').click(function(){
       if(wheelPosition === 0){
@@ -84,4 +85,28 @@ window.onload = function () {
         wheelPosition = 180;
       }
     }
-};
+  $('.pour') //Pour Me Another Drink, Bartender!
+
+    .delay(0)
+    .animate({
+      height: '22em'
+      }, 1500)
+    .delay(100)
+    .slideUp(2500);
+  $('#liquid') // I Said Fill 'Er Up!
+    .delay(400)
+    .animate({
+      'background-color': '#edaf32',
+      border: '10px solid #edaf32',
+      top: '-36em',
+      bottom: '36em',
+      height: '22em'
+    }, 3600);
+
+  $('.beer-foam') // Keep that Foam Rollin' Toward the Top! Yahooo!
+    .delay(3200)
+    .animate({
+      bottom: '33em',
+      opacity: '1'
+      }, 2300);
+  });
